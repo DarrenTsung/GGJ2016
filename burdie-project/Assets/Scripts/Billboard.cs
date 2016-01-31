d65acc2c50d1d424f3827bf16b46749f4c86e305
Roger Cheng <talentjp@gmail.com>
2016-01-31 12:51:25 -0800

    LevelEditor and Twinkling Stars

--- burdie-project/Assets/Scripts/Billboard.cs
+++ burdie-project/Assets/Scripts/Billboard.cs
@@ -1,16 +1,57 @@
-﻿ using UnityEngine;
+﻿using DT; 
+using UnityEngine;
 using System.Collections;
 
 public class Billboard : MonoBehaviour
 {
-    public Camera m_Camera;
+    private Camera m_Camera;
+    private MeshRenderer _meshRenderer;   
+    public float twinkleFrequency = 1.0f;
+
+    void Awake(){
+        m_Camera = Camera.main; //Hard-code to the main camera
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer.sharedMaterials[0].SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, 1.0f));
+        _meshRenderer.sharedMaterials[1].SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, 0.0f));
+    }
 
     void Start(){
     }
 
     void Update()
     {
+        //Rotate the stars to face the camera
         transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
     }
+
+    public void AnimateStar(float duration){
+        this.StopAllCoroutines();
+        this.StartCoroutine(this.CrossFadeMaterials(twinkleFrequency, duration));
+    }
+
+    private IEnumerator CrossFadeMaterials(float frequency, float duration) {
+        //Initial states
+        float timeElapsed = 0.0f; 
+        bool toggle = false; 
+        for (float time = 0.0f; time < duration; time += Time.deltaTime) {           
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed > frequency)
+            {
+                timeElapsed -= frequency;
+                toggle = !toggle;
+            }                    
+            if (toggle)
+            {
+                _meshRenderer.sharedMaterials [0].SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, Easers.Ease(EaseType.QuadOut, 1.0f, 0.0f, timeElapsed, frequency)));
+                _meshRenderer.sharedMaterials [1].SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, Easers.Ease(EaseType.QuadOut, 0.0f, 1.0f, timeElapsed, frequency)));
+            } else
+            {
+                _meshRenderer.sharedMaterials [0].SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, Easers.Ease(EaseType.QuadOut, 0.0f, 1.0f, timeElapsed, frequency)));
+                _meshRenderer.sharedMaterials [1].SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, Easers.Ease(EaseType.QuadOut, 1.0f, 0.0f, timeElapsed, frequency)));
+            }              
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
 }
--- burdie-project/Assets/StarSpawner.cs
+++ burdie-project/Assets/StarSpawner.cs
@@ -1,25 +1,131 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
 
 public class StarSpawner : MonoBehaviour {
+    [System.Serializable]
+    public class StarParams
+    {
+        public StarEnum starType;
+        public int duration;
+        public int pause;
+        public bool randomSpawn;
+    }
+
+    [System.Serializable]
+    public class Level
+    {
+        public StarParams[] stars;
+    }
+        
+    public Level[] levels;
+
+    public enum StarEnum : int {
+        A = 0,
+        B,
+        X,
+        Y
+    };       
+
+    public Transform starholder;
+    public GameObject starPrefabA;
+    public GameObject starPrefabB;
+    public GameObject starPrefabX;
+    public GameObject starPrefabY;
     public float distance;
-
-	// Use this for initialization
-    void Start () {
-        for (int i = 0; i < 10; i++)
-        {
-            Vector3 spawnPos = Camera.main.transform.position + GetRandomQuaternion() * (Vector3.up * distance);
-
-        }
+    public float minAngle = 20.0f;
+    public float maxAngle = 90.0f;
+    private float timeElapsed = 0.0f;
+    private float nextStarTrigger = 0;   
+    private float destroyTime = 0;
+    private int currentIdx = 0;
+    private int m_currentLevel = 0;
+    private GameObject m_lastStar = null;
+   
+    void Start () { 
 	}
+        
+    List<List<int>> ArrFromString(string[] levelStr){
+        List<List<int>> level = new List<List<int>>();
+        foreach (string numbers in levelStr)
+        {            
+            List<int> current = new List<int>();
+
+            foreach(string number in numbers.Split(','))
+            {
+                int num = 0;
+                if (current.Count == 0)
+                {
+                    num = (int)Enum.Parse(typeof(StarEnum), number);
+                } else
+                {
+                    num = Int32.Parse(number.Trim());
+                }
+                current.Add(num);
+            }
+            level.Add(current);
+        }
+        return level;
+    }
 	
 	// Update is called once per frame
-	void Update () {	
-	}
-
+	void Update () 
+    {               
+        timeElapsed += Time.deltaTime;       
+        if (timeElapsed > nextStarTrigger)
+        {           
+            if (currentIdx < levels [m_currentLevel].stars.GetLength(0))
+            {
+                destroyTime = levels [m_currentLevel].stars [currentIdx].duration;
+                nextStarTrigger = levels [m_currentLevel].stars [currentIdx].duration + levels [m_currentLevel].stars [currentIdx].pause; //Have to wait this long to create the next star
+                Vector3 spawnPos;
+                if (levels [m_currentLevel].stars [currentIdx].randomSpawn)
+                {
+                    spawnPos = Camera.main.transform.position + GetRandomQuaternion() * (Vector3.up * distance);
+                } else
+                {
+                    spawnPos = starholder.position;
+                }
+                GameObject star = null;
+                switch (levels [m_currentLevel].stars [currentIdx].starType)
+                {
+                    case StarEnum.A:
+                        star = (GameObject)GameObject.Instantiate(starPrefabA, spawnPos, Quaternion.identity);
+                        break;
+                    case StarEnum.B:
+                        star = (GameObject)GameObject.Instantiate(starPrefabB, spawnPos, Quaternion.identity);
+                        break;
+                    case StarEnum.X:
+                        star = (GameObject)GameObject.Instantiate(starPrefabX, spawnPos, Quaternion.identity);
+                        break;
+                    case StarEnum.Y:
+                        star = (GameObject)GameObject.Instantiate(starPrefabY, spawnPos, Quaternion.identity);
+                        break;
+                }
+                if (star != null)
+                {
+                    star.GetComponent<Billboard>().AnimateStar(levels [m_currentLevel].stars [currentIdx].duration);
+                }
+                timeElapsed = 0.0f;
+                currentIdx++;
+                m_lastStar = star;
+            } 
+            else
+            {
+                m_currentLevel++;
+                currentIdx = 0;
+            }
+        }
+        if (timeElapsed > destroyTime)
+        {
+            Destroy(m_lastStar);
+        }
+	}        
     Quaternion GetRandomQuaternion(){
-        float xRot = Random.Range(20.0f, 90.0f);
-        float yRot = Random.Range(0, 360.0f);
+        float xRot = UnityEngine.Random.Range(minAngle, maxAngle);
+        float yRot = UnityEngine.Random.Range(0, 360.0f);
         return Quaternion.Euler(new Vector3(xRot, yRot, 0));
-    }
+    }        
 }
