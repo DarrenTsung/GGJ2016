691e9cdbabc1839315bc56cb0c350c5a4e1ed6a1
Darren Tsung <darren@storm8.com>
2016-01-30 18:09:08 -0800

    Add generated wwise mac and also project

---
+++ burdie-project/Assets/Code/AppInit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AppInit : MonoBehaviour {
+    private void Start() {
+        AkSoundEngine.PostEvent("Play_Music", null);
+    }
+}
